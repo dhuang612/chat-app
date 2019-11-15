55dcc4973f792e72430953f6ca718224754b909c
dhuang612 <dhuang@madisoncollege.edu>
2019-11-15 14:09:51 -0500

    setup websockets

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,31 @@
-io()
+const socket = io()
+
+socket.on('welcome', (message) => {
+    console.log(message)
+})
+
+// socket.on('countUpdated', (count) => {
+//     console.log('the count has been updated', count)
+// })
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//     console.log('clicked')
+//     socket.emit('increment')
+// })
+
+// socket.on('message', (sendMessage) => {
+//     console.log(sendMessage)
+//     socket.emit('message', sendMessage)
+// })
+
+
+document.querySelector('#form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const sendMessage = e.target.elements.message.value
+    console.log(sendMessage)
+   socket.emit('sendMessage', message)
+})
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -13,9 +13,29 @@ const publicDirectoryPath = path.join(__dirname , '../public/');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+// server (emit) => client (receive) - countUpdated
+// client (emit) => server (receive) - increment
+ 
+let message = "Welcome!";
+
+io.on('connection', (socket) => {
     console.log('New WebSocket connection')
+    socket.emit('welcome', message)
+    // socket.emit('countUpdated', count)
+
+    // socket.on('increment', () => {
+    //     count++
+    //     io.emit('countUpdated', count)
+    // })
+
+    socket.on('sendMessage', (message) => {
+        console.log(message)
+        io.emit('sendMessage', message);
+    })
 })
+
+
+
 
 server.listen(port, () => {
     console.log(`Server is up on port: ${port}`);
